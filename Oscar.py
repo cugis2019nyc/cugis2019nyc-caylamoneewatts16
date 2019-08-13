2d0deacd6e534a27a3c0758e349677935ff4ecd7
caylamoneewatts16 <54040179+caylamoneewatts16@users.noreply.github.com>
2019-08-13 14:59:59 -0400

    Add files via upload

--- Oscar.py
+++ Oscar.py
@@ -10,4 +10,34 @@ def time(a,b,h):
     print("the area of a triangle with base is",time)
    
 time(1/2,12,24)
- 
+chocolate1 = "milk"
+chocolate2 = "dark"
+chocolate3 = "white"
+
+print(chocolate1)
+print("chocolate1")
+
+milk =3
+dark =6
+white =9
+
+milk = milk+3
+print(milk+20)
+
+dark = dark+6
+print(dark+6)
+
+white = white+9
+print(white+9)
+
+import math
+dir(math)
+
+math.factorial(7)
+
+math.sqrt(350)
+math.exp(2)
+math.pow(2.718281828459045,2)
+math.pow(35,9)
+(35**9)
+math.pi
