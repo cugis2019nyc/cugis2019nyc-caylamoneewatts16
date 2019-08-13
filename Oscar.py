ecc86f68fc5112065d49029e322191b0bc1aa733
caylamoneewatts16 <54040179+caylamoneewatts16@users.noreply.github.com>
2019-08-13 13:43:48 -0400

    Add files via upload

---
+++ Oscar.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 10:33:52 2019
+
+@author: Columbia Girls
+"""
+
+def time(a,b,h):
+    time=(1/2)*b*h 
+    print("the area of a triangle with base is",time)
+   
+time(1/2,12,24)
+ 
